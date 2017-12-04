a1c13c095b852a29ba80ac330e6641ead7a2ce20
HelenvG <helenvangils@live.nl>
2017-12-04 10:30:13 +0100

    assignment 23 completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -2,9 +2,9 @@ var pointsArray = document.getElementsByClassName('point');
 
 var revealPoint = function(point) {
   point.style.opacity = 1;
-  point.style.transform = "scaleX(1) translateY(0)";
+  point.style.transform = 'scaleX(1) translateY(0)';
   point.style.msTransform = "scaleX(1) translateY(0)";
-  points.style.WebkitTransform = "scaleX(1) translateY(0)";
+  point.style.WebkitTransform = "scaleX(1) translateY(0)";
 };
 
 var animatePoints = function (points) {
