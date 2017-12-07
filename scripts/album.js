47dbe14058550ef761a2ccf1e359e055a20952b1
HelenvG <helenvangils@live.nl>
2017-12-07 16:00:56 +0100

    checkpoint 26 dom: completed

--- scripts/album.js
+++ scripts/album.js
@@ -76,8 +76,18 @@ var setCurrentAlbum = function(album) {
   }
 };
 
+var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
+
+var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+
 window.onload = function() {
   setCurrentAlbum(albumPicasso);
+
+  songListContainer.addEventListener('mouseover', function(event) {
+    if (event.target.parentElement.className === 'album-view-song-item') {
+      event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+    }
+});
 
   var albums = [albumPicasso, albumMarconi, albumPorter];
   var i = 1;
