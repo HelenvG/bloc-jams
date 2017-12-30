d67efb9e9a2a674c0afe8ad30b3ec96ca97de8cf
HelenvG <helenvangils@live.nl>
2017-12-30 10:05:21 +0100

    checpoint 38 jquery: completed

---
+++ scripts/fixtures.js
@@ -0,0 +1,47 @@
+
+var albumPicasso = {
+  title: 'The Colors',
+  artist: 'Pablo Picasso',
+  label: 'Cubism',
+  year: '1881',
+  albumArtUrl: 'assets/images/album_covers/01.png',
+  songs: [
+    { title: 'Blue', duration: '4:26' },
+    { title: 'Green', duration: '3:14' },
+    { title: 'Red', duration: '5:01' },
+    { title: 'Pink', duration: '3:21'},
+    { title: 'Magenta', duration: '2:15'}
+  ]
+};
+  
+var albumMarconi = {
+  title: 'The Telephone',
+  artist: 'Guglielmo Marconi',
+  label: 'EM',
+  year: '1909',
+  albumArtUrl: 'assets/images/album_covers/20.png',
+  songs: [
+    { title: 'Hello, Operator?', duration: '1:01' },
+    { title: 'Ring, ring, ring', duration: '5:01' },
+    { title: 'Fits in your pocket', duration: '3:21'},
+    { title: 'Can you hear me now?', duration: '3:14' },
+    { title: 'Wrong phone number', duration: '2:15'}
+  ]
+};
+  
+var albumPorter = {
+  title: 'Water',
+  artist: 'Gregory Porter',
+  label: 'Motéma Music',
+  year: '1909',
+  albumArtUrl: 'assets/images/album_covers/03.png',
+  songs: [
+    { title: 'Illusion', duration: '2:21' },
+    { title: 'Pretty', duration: '3:56' },
+    { title: 'Magic Cup', duration: '3:02'},
+    { title: 'Skylark', duration: '4:04' },
+    { title: 'Black Nile', duration: '2:59'},
+    { title: 'Wisdom', duration: '3:76'},
+    { title: '1960 What?', duration: '2:88'}
+  ]
+};
