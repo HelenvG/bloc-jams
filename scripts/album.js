93e3479e4240be582ebd5b45449c695c9fa432cd
HelenvG <helenvangils@live.nl>
2018-01-04 14:23:37 +0100

    assignment 39 completed

--- scripts/album.js
+++ scripts/album.js
@@ -165,6 +165,26 @@ var updatePlayerBarSong = function() {
   $('.main-controls .play-pause').html(playerBarPauseButton);
 };
 
+var togglePlayFromPlayerbar = function() {
+
+  var $currentlyPlayingCell = getSongNumberCell(currentlyPlayingSongNumber);
+  if (currentSoundFile.isPaused()) { //If a song is paused and the play button is clicked in the player bar, it will
+    //Change the song number cell from a play button to a pause button
+    $currentlyPlayingCell.html(pauseButtonTemplate);
+    //Change the HTML of the player bar's play button to a pause button
+    $(this).html(playerBarPauseButton);
+    //Play the song
+    currentSoundFile.play();
+  } else if (currentSoundFile) { //If the song is playing (so a current sound file exist), and the pause button is clicked
+    //Change the song number cell from a pause button to a play button
+    $currentlyPlayingCell.html(playButtonTemplate);
+    //Change the HTML of the player bar's pause button to a play button
+    $(this).html(playerBarPlayButton);
+    //Pause the song
+    currentSoundFile.pause();
+  }
+};
+
 var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
 var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
 var playerBarPlayButton = '<span class="ion-play"></span>';
@@ -177,10 +197,12 @@ var currentSoundFile = null;
 var currentVolume = 80;
 var $previousButton = $('.main-controls .previous');
 var $nextButton = $('.main-controls .next');
+var $playPauseButton = $('.main-controls .play-pause');
 
 $(document).ready(function() {
   setCurrentAlbum(albumPicasso);
   $previousButton.click(previousSong);
   $nextButton.click(nextSong);
+  $playPauseButton.click(togglePlayFromPlayerbar);
 });
 
